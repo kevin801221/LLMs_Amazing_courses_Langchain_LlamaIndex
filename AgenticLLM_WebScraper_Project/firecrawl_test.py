892934fe4600571bab8c6aded0e0379b0284c7ef
kevin801221 <kilong31442@gmail.com>
2024-11-17 13:31:31 +0800

    apikey

--- AgenticLLM_WebScraper_Project/firecrawl_test.py
+++ AgenticLLM_WebScraper_Project/firecrawl_test.py
@@ -1,6 +1,6 @@
 from firecrawl import FirecrawlApp
 
-app = FirecrawlApp(api_key="fc-d92bfd051afd4a91b71f48c78aa9b871")
+app = FirecrawlApp(api_key="")
 
 # Scrape a website:
 scrape_result = app.crawl_url('https://medium.com/kaggle-blog', params={'formats': ['markdown', 'html']})
